61881d2482f32323b0914d4a306e462ecd61aa2c
Harsh-26626 <harsh.ahuja.cs24@ggits.net>
2024-12-20 01:16:10 +0530

    Changed Loginpages

--- Homepage/js/directory.js
+++ Homepage/js/directory.js
@@ -6,13 +6,14 @@ async function fetchAlumni() {
         const alumniContainer = document.getElementById('alumniContainer');
         alumniData.forEach(alumni => {
             const alumniCard = document.createElement('div');
+            alumniCard.onclick = () => storeEmail(`${alumni.email}`);
             alumniCard.classList.add('alumni-card');
             alumniCard.innerHTML = `
-                <img class="alumni-image" src="${alumni.image}" alt="${alumni.name}">
+                <img class="profile-photo" src="${alumni.profilePic}" alt="${alumni.name}">
                 <div class="alumni-info">
                     <h3>${alumni.name}</h3>
                     <p>${alumni.college}</p>
-                    <p>${alumni.branch} - ${alumni.year}</p>
+                    <p>${alumni.branch} - Batch of ${alumni.year}</p>
                 </div>
             `;
             alumniContainer.appendChild(alumniCard);
@@ -21,5 +22,11 @@ async function fetchAlumni() {
         console.error('Error fetching alumni data:', error);
     }
 }
+
+function storeEmail(input) {
+    console.log("Input value:", input);
+    localStorage.setItem('input', input);
+    window.location.href = "viewprofile.html";
+    }
 
 fetchAlumni();
