a7bbf49858e1a73d9706fbbe94709d7b1d134e5a
Abhiyanshu-Sharma <abhiyanshusharma010305@gmail.com>
2024-12-20 14:15:41 +0530

    category button

--- Homepage/js/chat.js
+++ Homepage/js/chat.js
@@ -1,4 +1,4 @@
-const socket = io('https://chat-5rqk.onrender.com');
+const socket = io('http://chat-production-2e8d.up.railway.app');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageimp');
