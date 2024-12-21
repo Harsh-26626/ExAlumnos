a48be0b8a3bfe84440a8fee5ad4f5b4482bc8201
Harsh-26626 <harsh.ahuja.cs24@ggits.net>
2024-12-21 09:29:09 +0530

    added subscribe

--- Homepage/js/header2.js
+++ Homepage/js/header2.js
@@ -4,9 +4,10 @@ function logout() {
     localStorage.setItem('username', "Guest");
 localStorage.setItem('email', "Guest@ExAlumnos.com");
 localStorage.setItem('college', "Gyan Ganga Group");
-localStorage.setItem('branch', "");
-localStorage.setItem('year', "");
+localStorage.setItem('branch', "unknown");
+localStorage.setItem('year', "2003 - 2028");
 localStorage.setItem('photo', result.user.profilePic);
 localStorage.setItem('banner', result.user.bannerPic);
 console.log("username:", localStorage.getItem('username'));
+window.location.href = "index.html";
 }
